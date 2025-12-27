e96ffdf769eeffc6a20ebfbbe0afa7dd73891be3
Svante Stadler <svante.stadler@gmail.com>
2025-12-27 16:42:12 +0100

    return number of used mousedowns

--- IGraphics/IGraphics.cpp
+++ IGraphics/IGraphics.cpp
@@ -1059,7 +1059,7 @@ int IGraphics::OnMouseDown(const std::vector<IMouseInfo>& points)
       {
         ReleaseMouseCapture();
         PopupHostContextMenuForParam(pCapturedControl, paramIdx, x, y);
-        return;
+        return 1;
       }
 #endif
 
